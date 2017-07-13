75b85c935e86ef2ee7d17007ad1cdf91c2282d49
Rashed Abdel-Tawab <rashed@linux.com>
2017-07-13 02:38:58 -0400

    Update some ACPI tables to fix USB and add HDMI fix script

---
+++ hdmi.sh
@@ -0,0 +1,2 @@
+ sudo sed -i.vanilla 's/Mac-FC02E91DDD3FA6A4/Mac-A5C67F76ED83108C/g' /System/Library/Extensions/AppleGraphicsControl.kext/Contents/PlugIns/AppleGraphicsDevicePolicy.kext/Contents/Info.plist
+
