be6b32e84972dcee6336065260565693e67573ea
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-01 02:26:38 -0400

    Update apfs.efi from HS release and rebuilt kext cache on HDMI change

--- hdmi.sh
+++ hdmi.sh
@@ -1,2 +1,2 @@
- sudo sed -i.vanilla 's/Mac-FC02E91DDD3FA6A4/Mac-A5C67F76ED83108C/g' /System/Library/Extensions/AppleGraphicsControl.kext/Contents/PlugIns/AppleGraphicsDevicePolicy.kext/Contents/Info.plist
-
+sudo sed -i.vanilla 's/Mac-FC02E91DDD3FA6A4/Mac-A5C67F76ED83108C/g' /System/Library/Extensions/AppleGraphicsControl.kext/Contents/PlugIns/AppleGraphicsDevicePolicy.kext/Contents/Info.plist
+sudo touch /System/Library/Extensions && sudo kextcache -u /
